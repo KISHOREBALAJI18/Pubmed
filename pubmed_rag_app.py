c946796d1f40dcb4dcb1a546061d9e7885b5fd2f
KISHOREBALAJI18 <royalkishore37@gmail.com>
2025-06-04 00:30:22 +0530

    Update pubmed_rag_app.py

--- pubmed_rag_app.py
+++ pubmed_rag_app.py
@@ -1,5 +1,3 @@
-pip install Bio openai requests chromadb langchain_google_genai 
-pip install -U langchain-community 
 import streamlit as st
 import requests
 from xml.etree import ElementTree as ET
